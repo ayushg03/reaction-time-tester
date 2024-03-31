65c643d65e8a9e648b6b5106c17d7aed1578ff4b
ayushg03 <20cs3016@rgipt.ac.in>
2024-03-31 10:45:40 +0530

    Few fixes and updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, IconButton, Modal, Typography } from '@mui/material';
+import { Box, Button, IconButton, Modal, Typography, LinearProgress } from '@mui/material';
 import Circle from './Circle';
 import CloseIcon from '@mui/icons-material/Close';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true); // Initial modal open
+  const [showResults, setShowResults] = useState(false); // Test results modal
   const [testResults, setTestResults] = useState({
     correctAnswers: 0,
     incorrectAnswers: 0,
@@ -19,22 +20,17 @@ function App() {
     let intervalId;
     if (isTestRunning) {
       intervalId = setInterval(() => {
-        setTimer((prevTimer) =>{
+        setTimer((prevTimer) => {
           if (prevTimer === 0) {
             clearInterval(intervalId);
-            setIsOpen(true);
+            setIsOpen(false); // Close instructions modal
+            setShowResults(true); // Show test results modal
             setIsTestRunning(false);
-            setTestStarted(false);
+            setTestStarted(false); // Reset test started flag
             return 0;
           }
-          return prevTimer - 1
+          return prevTimer - 1;
         });
-        if (timer === 0) {
-          clearInterval(intervalId);
-          setIsOpen(true);
-          setIsTestRunning(false);
-          setTestStarted(false); // Reset test started flag
-        }
       }, 1000);
     }
 
@@ -46,6 +42,7 @@ function App() {
   const handleStart = () => {
     setIsTestRunning(true);
     setTestStarted(true); // Test has been started
+    setIsOpen(false); // Close instructions modal
   };
 
   const handlePause = () => {
@@ -53,7 +50,7 @@ function App() {
   };
 
   const handleReset = () => {
-    setIsOpen(false);
+    setShowResults(false);
     setIsTestRunning(false);
     setTimer(60);
     setTestResults({
@@ -68,7 +65,7 @@ function App() {
   const handleColorSelection = (color) => {
     // Calculate elapsed time for the current question
     const elapsedTime = 60 - timer;
-  
+
     // Update test statistics based on color selection
     setTestResults((prevResults) => ({
       ...prevResults,
@@ -77,11 +74,10 @@ function App() {
       incorrectAnswers: color === 'correct' ? prevResults.incorrectAnswers : prevResults.incorrectAnswers + 1,
     }));
   };
-  
 
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <Typography variant="h5" style={{ marginTop: '20px' }}>
+      <Typography variant="h5" style={{ marginTop: '20px', color: timer <= 10 ? 'red' : 'inherit' ,fontFamily:'poppins',  fontSize: '1.8em', fontWeight: '500'}}>
         Time Left: {timer} seconds
       </Typography>
       {isTestRunning && (
@@ -93,18 +89,24 @@ function App() {
         <Button
           variant="contained"
           onClick={isTestRunning ? handlePause : handleStart}
-          size='large'
+          size="large"
           sx={{
             minWidth: '150px',
             minHeight: '50px',
             fontSize: '1.5rem',
+            fontFamily:'poppins',
+            backgroundColor: '#01959a',
+            "&:hover": {
+              backgroundColor: '#0c8185', // Set the same background color for hover state
+            }
           }}
         >
           {isTestRunning ? 'Pause Test' : (testStarted ? 'Resume Test' : 'Start Test')}
         </Button>
+       
       </Box>
-      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-        <div style={{ width: '300px', margin: 'auto', marginTop: '200px', padding: '20px', backgroundColor: '#fff', borderRadius: '10px', position: 'relative' }}>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)} >
+        <div style={{ width: '40vw', margin: 'auto', marginTop: '200px', padding: '20px', backgroundColor: '#fff', borderRadius: '10px', position: 'relative' ,border:'none',outline: 'none'}}>
           <IconButton
             aria-label="close"
             style={{ position: 'absolute', top: '5px', right: '5px' }}
@@ -112,14 +114,51 @@ function App() {
           >
             <CloseIcon />
           </IconButton>
-          <Typography variant="h5" gutterBottom align="center">
-           Test Results
+          <Typography variant="h5" gutterBottom align="center" sx={{fontFamily:'poppins',  fontSize: '1.8em'}}>
+           <u>Instructions</u> 
           </Typography>
-          <Typography>Correct Answers: {testResults.correctAnswers}</Typography>
-          <Typography>Incorrect Answers: {testResults.incorrectAnswers}</Typography>
-          <Typography>Total Questions Answered: {testResults.totalQuestionsAnswered}</Typography>
+          <Typography variant="body1" gutterBottom align="left" sx={{fontFamily:'poppins',  fontSize: '1.2em'}}>
+            1. Press 'L' to load the color on screen.
+          </Typography>
+          <Typography variant="body1" gutterBottom align="left" sx={{fontFamily:'poppins',  fontSize: '1.2em'}}>
+            2. Press 'R', 'G', 'Y', and 'B' buttons corresponding to the color on the screen to answer.
+          </Typography>
+          <Typography variant="body1" gutterBottom align="left" sx={{fontFamily:'poppins',  fontSize: '1.2em'}}>
+            3. The total time of the test is 1 minute.
+          </Typography>
+          <Typography variant="body1" gutterBottom align="left" sx={{fontFamily:'poppins',  fontSize: '1.2em'}}>
+            4. Once the timer expires, results will be shown on the screen.
+          </Typography>
+          <div style={{ textAlign: 'center', marginTop: '20px' }} >
+            <Button variant="contained" onClick={() => setIsOpen(false)} sx={{fontFamily:'poppins',  fontSize: '1.2em', minWidth: '60px', minHeight: '15px',backgroundColor: '#01959a',
+            "&:hover": {
+              backgroundColor: '#0c8185', // Set the same background color for hover state
+            }}}>
+              Proceed
+            </Button>
+          </div>
+        </div>
+      </Modal>
+      <Modal open={showResults} onClose={() => setShowResults(false)}>
+        <div style={{ width: '300px', margin: 'auto', marginTop: '200px', padding: '20px', backgroundColor: '#fff', borderRadius: '10px', position: 'relative',outline: 'none' }}>
+          <IconButton
+            aria-label="close"
+            style={{ position: 'absolute', top: '5px', right: '5px' }}
+            onClick={() => setShowResults(false)}
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography variant="h5" gutterBottom align="center" sx={{fontFamily:'poppins',  fontSize: '1.5em'}}>
+            <u>Test Results</u>
+          </Typography>
+          <Typography sx={{fontFamily:'poppins',  fontSize: '1.2em'}}>Correct Answers: {testResults.correctAnswers}</Typography>
+          <Typography sx={{fontFamily:'poppins',  fontSize: '1.2em'}}>Incorrect Answers: {testResults.incorrectAnswers}</Typography>
+          <Typography sx={{fontFamily:'poppins',  fontSize: '1.2em'}}>Total Questions Answered: {testResults.totalQuestionsAnswered}</Typography>
           <div style={{ textAlign: 'center', marginTop: '20px' }}>
-            <Button variant="contained" onClick={handleReset}>
+            <Button variant="contained" onClick={handleReset} sx={{fontFamily:'poppins',  fontSize: '1.2em', minWidth: '60px', minHeight: '15px',backgroundColor: '#01959a',
+            "&:hover": {
+              backgroundColor: '#0c8185', // Set the same background color for hover state
+            }}}>
               Start New Test
             </Button>
           </div>
